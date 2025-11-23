82328545a0cd1782864647ca96e04cc2a552ff8b
Michal Wasiak <michal.wasiak@hotmail.com>
2025-11-23 11:38:24 +1000

    test: throw error when min > max

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,9 +4,14 @@ describe('randomNumber', () => {
     it('returns 1 when range is 1 to 1', () => {
         expect(randomNumber(1, 1)).toBe(1);
     });
+
     it('throws error when arguments are not numbers', () => {
         expect(() => randomNumber('a', 5)).toThrow();
         expect(() => randomNumber(1, null)).toThrow();
         expect(() => randomNumber(undefined, 10)).toThrow();
-    })
+    });
+
+    it('throws error when min is grater than max', () => {
+        expect(() => randomNumber(10, 5)).toThrow();
+    });
 });
