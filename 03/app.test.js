3dd90688b0849bcd8d79d1184aa002bf55f473b5
Michal Wasiak <michal.wasiak@hotmail.com>
2025-11-23 11:33:21 +1000

    test: throw error when arguments are not numbers

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,7 +1,12 @@
 import randomNumber from './app';
 
 describe('randomNumber', () => {
-    test('returns 1 when range is 1 to 1', () => {
+    it('returns 1 when range is 1 to 1', () => {
         expect(randomNumber(1, 1)).toBe(1);
     });
+    it('throws error when arguments are not numbers', () => {
+        expect(() => randomNumber('a', 5)).toThrow();
+        expect(() => randomNumber(1, null)).toThrow();
+        expect(() => randomNumber(undefined, 10)).toThrow();
+    })
 });
