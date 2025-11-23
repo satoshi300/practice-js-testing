69cc757eefae2ee3fc467b93e1750f8edba7fd25
Michal Wasiak <michal.wasiak@hotmail.com>
2025-11-23 11:40:22 +1000

    validate min <= max

--- 03/app.js
+++ 03/app.js
@@ -26,5 +26,9 @@ export default function randomNumber(min, max) {
     if (typeof min !== 'number' || typeof max !== 'number') {
         throw new Error('Arguments must be a number')
     }
+
+    if (min > max) {
+        throw new Error('Min cannot be grater than max');
+    }
     return 1;
 }
